807b32b959649b6c45f34f0fd4f439e7a6c7c658
RSLilova18 <RSLilova18@codingburgas.bg>
2022-12-05 12:23:48 +0200

    lessons 3-43 to 3-50

--- src/main/java/com/example/springmvc/AthleteController.java
+++ src/main/java/com/example/springmvc/AthleteController.java
@@ -1,4 +1,5 @@
 package com.example.springmvc;
+import com.example.springmvc.model.Athlete;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
--- src/main/java/com/example/springmvc/SpringmvcApplication.java
+++ src/main/java/com/example/springmvc/SpringmvcApplication.java
@@ -3,11 +3,12 @@ package com.example.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class SpringmvcApplication {
+public class SpringMvcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringmvcApplication.class, args);
+        SpringApplication.run(SpringMvcApplication.class, args);
     }
 
 }
